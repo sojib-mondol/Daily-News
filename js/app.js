12277a61e8d652d63f82cccbfaf82f85159584e2
sojib-sudo <sojibmandol@gmail.com>
2022-09-03 10:38:41 +0600

    nav added & files are added

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const loadCategory = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+loadCategory();
