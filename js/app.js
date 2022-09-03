bd1228d1f41e528c49539d845b78817f4c2d5ad0
sojib-sudo <sojibmandol@gmail.com>
2022-09-03 12:06:47 +0600

    added news catagorys

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 const loadCategory = () => {
     fetch('https://openapi.programming-hero.com/api/news/categories')
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => displayCategory(data.data.news_category));
+}
+
+const displayCategory = (categories) => {
+    const newsCategoryField = document.getElementById('news-catagory-field')
+    categories.forEach(element => {
+        console.log(element);
+        const newsCategoryNamesDiv = document.createElement('div');
+        newsCategoryNamesDiv.innerHTML = ` 
+           <button type="button" class="btn-style btn-hover">${element.category_name}</button> 
+       ` 
+       newsCategoryField.appendChild(newsCategoryNamesDiv);
+    });
 }
 
 loadCategory();
